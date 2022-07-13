ade1ceeb04e5836d62d659ec0f269688d7e327ab
Adam Anderson <adamcanderson400@gmail.com>
2022-07-12 23:42:06 -0700

    Time: 57 ms (32.60%), Space: 13.9 MB (58.40%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,9 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # convert the list to a integer
         num = 0
         for i in range(len(digits)):
             num += 10**i * digits[::-1][i]
-            
-        num += 1
         
-        return list(str(num))
+        # Add one to the number and convert it back to a list
+        return list(str(num + 1))
