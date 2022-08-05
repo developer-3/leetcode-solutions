0a2309796a617c70fb0e1324db6ff0933d3dbd81
Adam Anderson <adamcanderson400@gmail.com>
2022-08-05 11:53:09 -0700

    Time: 456 ms (59.15%), Space: 24.9 MB (11.82%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,52 @@
+class Solution:
+    def dfs(self, grid, visited, r, c, n, m):
+        res = [0]
+        
+        def helper(i, j):
+            if grid[i][j] == "0":
+                return
+            
+            if (i, j) in visited:
+                return 
+            
+            if grid[i][j] == "1":
+                visited[(i, j)] = True
+                res[0] += 1
+            
+            # up
+            if i > 0:
+                helper(i-1, j)
+                
+            if j < m-1:
+                helper(i, j+1)
+                
+            #down
+            if i < n - 1:
+                helper(i+1, j)
+                
+            # left
+            if j > 0:
+                helper(i, j-1)
+        
+        helper(r, c)
+        return [visited, res]
+            
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n, m = len(grid), len(grid[0])
+        visited = defaultdict(set)
+        
+        res = []
+        
+        for r in range(n):
+            for c in range(m):
+                if (r,c) not in visited and grid[r][c] == "1":
+                    # search for island size
+                    search = self.dfs(grid, visited, r, c, n, m)
+                    visited = search[0]
+                    res.append(search[1])
+
+
+        return len(res)
+                    
+                    
